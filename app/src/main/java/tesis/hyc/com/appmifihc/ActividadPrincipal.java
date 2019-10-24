5b695708ff95aab07a5a7f3161c78002f1bf39ec
Hans <ache.cg@gmail.com>
2019-10-24 00:39:16 -0500

    Initial commit

---
+++ app/src/main/java/tesis/hyc/com/appmifihc/ActividadPrincipal.java
@@ -0,0 +1,14 @@
+package tesis.hyc.com.appmifihc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_principal);
+    }
+}
